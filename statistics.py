3de51bc5395dd712272f0a6457cabee1222fbfc8
mhackel <m.hackel999@gmail.com>
2025-05-28 16:59:17 -0500

    POS tagging with spacy

---
+++ statistics.py
@@ -0,0 +1,21 @@
+# spacy, nltk
+import spacy
+import nltk
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+words = ["I", "9", "Boy", "Bitter"]
+words_with_pos = {}
+
+# POS tagging
+nlp = spacy.load('en_core_web_sm')
+for word in words:
+    tag = nlp(word)
+    for token in tag:
+        # print(token.text, token.pos_)
+        words_with_pos[token.text] = token.pos_
+
+print(words_with_pos)
+# Statistics
+# Pie chart
